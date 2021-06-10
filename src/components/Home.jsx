fd79f57d9c4973858602f9769fe6e6de8f09929b
Nathan Phame <diyf2021@gmail.com>
2021-06-10 13:16:53 -0400

    Added Axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,123 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Home() {
+  const [pet, setPet] = useState([])
+
+  useEffect(async function () {
+    const response = await axios.get(
+      'https://tamagotchinate.herokuapp.com/api/Pets'
+    )
+
+    setPet(response.data)
+  }, [])
+
   return (
     <>
       <h2 className="title">Current Pets</h2>
       <div className="pets">
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
-        <ul>
-          <li className="name">
-            <Link to="/1">Pet Name</Link>
-          </li>
-          <li>Birthday</li>
-          <li>Hunger</li>
-          <li>Happiness</li>
-        </ul>
         <ul>
           <li className="name">
             <Link to="/1">Pet Name</Link>
